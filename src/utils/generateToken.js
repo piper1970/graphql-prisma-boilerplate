61225f1bfe01819d6f1bb8017bbe3d44f08f0c3a
Steve Sargent <gs_sarge@yahoo.com>
2019-01-04 06:17:05 -0800

    Put JWT secret in configs

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken';
 
+const secret = process.env.JWT_SECRET;
 const generateToken = userId => {
-    return jwt.sign({userId}, 'someSecret', {
+    return jwt.sign({userId}, secret, {
         expiresIn: '1 hour'
     });
 };
--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
 
+const secret = process.env.JWT_SECRET;
 const getUserId = (request, requireAuth=true) => {
     const header = request.request? request.request.headers.authorization: request.connection.context.Authorization;
     if(header){
         const token = header.replace('Bearer ', '');
-        const verified = jwt.verify(token, 'someSecret');
+        const verified = jwt.verify(token, secret);
         return verified.userId;
     }
     if(requireAuth){
